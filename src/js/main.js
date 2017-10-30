5f9839698e080e602173564ecb31a5a555349dd1
dpmango <xs290@me.com>
2017-10-30 21:19:11 +0300

    up

--- src/js/main.js
+++ src/js/main.js
@@ -735,5 +735,49 @@ $(document).ready(function(){
   })
 
 
+  window.Share = {
+  	vkontakte: function(purl, ptitle, pimg, text) {
+  		url  = 'http://vkontakte.ru/share.php?';
+  		url += 'url='          + encodeURIComponent(purl);
+  		url += '&title='       + encodeURIComponent(ptitle);
+  		url += '&description=' + encodeURIComponent(text);
+  		url += '&image='       + encodeURIComponent(pimg);
+  		url += '&noparse=true';
+  		Share.popup(url);
+  	},
+  	odnoklassniki: function(purl, text) {
+  		url  = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
+  		url += '&st.comments=' + encodeURIComponent(text);
+  		url += '&st._surl='    + encodeURIComponent(purl);
+  		Share.popup(url);
+  	},
+  	facebook: function(purl, ptitle, pimg, text) {
+  		url  = 'http://www.facebook.com/sharer.php?s=100';
+  		url += '&p[title]='     + encodeURIComponent(ptitle);
+  		url += '&p[summary]='   + encodeURIComponent(text);
+  		url += '&p[url]='       + encodeURIComponent(purl);
+  		url += '&p[images][0]=' + encodeURIComponent(pimg);
+  		Share.popup(url);
+  	},
+  	twitter: function(purl, ptitle) {
+  		url  = 'http://twitter.com/share?';
+  		url += 'text='      + encodeURIComponent(ptitle);
+  		url += '&url='      + encodeURIComponent(purl);
+  		url += '&counturl=' + encodeURIComponent(purl);
+  		Share.popup(url);
+  	},
+
+  	popup: function(url) {
+  		window.open(url,'','toolbar=0,status=0,width=626,height=436');
+  	}
+  };
+
+  window.fbShare = function fbShare(siteUrl, picture, title, caption, descr, winWidth,  winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+
+    window.open('http://www.facebook.com/sharer.php?s=100&u=' + siteUrl + '&picture=' + picture + '&title=' + title + '&caption=' + caption , '&description=' + descr, 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+  }
+
 
 });
